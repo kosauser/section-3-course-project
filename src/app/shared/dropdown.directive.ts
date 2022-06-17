46464c35a53a208eaebfdd725c8f9f293d6951b5
Albert Kosiorowski <albert.kosiorowski@motorolasolutions.com>
2022-06-17 12:07:02 +0200

    Add dropdown using costom DIRECTIVE - Section 8

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2, } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective {
+    @HostBinding('class.open') isOpen = false;
+
+    @HostListener('click') toggleOpen(eventData: Event) {
+        this.isOpen = !this.isOpen;
+    }
+
+    // Closing the Dropdown From Anywhere
+    // constructor(private elRef: ElementRef) {}
+    //
+    // @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    //     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    // }
+}
